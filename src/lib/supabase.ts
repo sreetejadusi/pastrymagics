569b29e91efe7e28fd07059cbd48b3bb7619f8a9
Tedestudio <team.tedestudio@gmail.com>
2025-09-16 17:57:12 +0530

    Environment Variables
    Signed-off-by: Tedestudio <team.tedestudio@gmail.com>

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL as string | undefined;
+const SUPABASE_URL = process.env.SUPABASE_URL as string | undefined;
 const SUPABASE_PUBLISHABLE_KEY = process.env
-  .NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY as string | undefined;
+  .SUPABASE_PUBLISHABLE_KEY as string | undefined;
 
 export const supabase =
   SUPABASE_URL && SUPABASE_PUBLISHABLE_KEY
